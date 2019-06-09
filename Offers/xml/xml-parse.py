fccd37f9c3fcc34f3847dae32748c4e512deb1d5
Tushar Choudhary <tchoudhary99@gmail.com>
2019-06-09 11:59:06 +0530

    update case in parse script and add resultant csv

--- Offers/xml/xml-parse.py
+++ Offers/xml/xml-parse.py
@@ -1,26 +1,73 @@
 from bs4 import BeautifulSoup
 
 # read offer file
-offer_xml = open("Offers_06062018.xml", 'r').read()
+offer_xml = open("Offers_06062018.xml", 'r')
 
 # make BeautifulSoup object
 soup = BeautifulSoup(offer_xml, "lxml")
 
 data = soup.find_all('metadata')
 
-for datum in data:
-    title = datum.find("field", {'name':"sbi.offers.title"}).text
-    description = datum.find("field", {'name':"sbi.offers.description"}).text
-    offer_text = datum.find("field", {'name':"sbi.offers.offertext"}).text
-    is_online = datum.find("field", {'name':"sbi.offers.isonline"}).text
-    category = datum.find("field", {'name':"sbi.offers.category"}).text
-    start_date = datum.find("field", {'name':"sbi.offers.startdate"}).text
-    end_date = datum.find("field", {'name':"sbi.offers.enddate"}).text
+filename = "offers_xml.csv"
+f = open(filename, "w")
 
-    print("title:" + title)
-    print("description:" + description)
-    print("offer_text:" + offer_text)
-    print("is_online:" + is_online)
-    print("category:" + category)
-    print("start_date:" + start_date)
-    print("end_date:" + end_date)
+header = "title, description, offer_text, is_online, category, start_date, end_date\n"
+
+f.write(header)
+
+for datum in data:
+    if datum.find("field", {'name':"sbi.offers.title"}) == None:
+        title = ""
+    else:
+        title = datum.find("field", {'name':"sbi.offers.title"}).text
+
+    if datum.find("field", {'name':"sbi.offers.description"}) == None:
+        description = ""
+    else:
+        description = datum.find("field", {'name':"sbi.offers.description"}).text
+
+    if datum.find("field", {'name':"sbi.offers.offertext"}) == None:
+        offer_text = ""
+    else:
+        offer_text = datum.find("field", {'name':"sbi.offers.offertext"}).text
+
+    if datum.find("field", {'name':"sbi.offers.isonline"}) == None:
+        is_online = ""
+    else:
+        is_online = datum.find("field", {'name':"sbi.offers.isonline"}).text
+
+    if datum.find("field", {'name':"sbi.offers.category"}) == None:
+        category = ""
+    else:
+        category = datum.find("field", {'name':"sbi.offers.category"}).text
+
+    if datum.find("field", {'name':"sbi.offers.startdate"}) == None:
+        start_date = ""
+    else:
+        start_date = datum.find("field", {'name':"sbi.offers.startdate"}).text
+
+    if datum.find("field", {'name':"sbi.offers.enddate"}) == None:
+        end_date = ""
+    else:
+        end_date = datum.find("field", {'name':"sbi.offers.enddate"}).text
+
+    f.write(title + "," + description.replace( ",", " ") + "," + offer_text.replace( ",", " ") + "," + is_online + "," + category.replace( ",", "|") + "," + start_date + "," + end_date + "\n")
+
+f.close()
+
+    # description = datum.find("field", {'name':"sbi.offers.description"}).text
+    # offer_text = datum.find("field", {'name':"sbi.offers.offertext"}).text
+    # is_online = datum.find("field", {'name':"sbi.offers.isonline"}).text
+    # category = datum.find("field", {'name':"sbi.offers.category"}).text
+    # start_date = datum.find("field", {'name':"sbi.offers.startdate"}).text
+    # end_date = datum.find("field", {'name':"sbi.offers.enddate"}).text
+
+    # print("title:" + title)
+    # print("description:" + description)
+    # print("offer_text:" + offer_text)
+    # print("is_online:" + is_online)
+    # print("category:" + category)
+    # print("start_date:" + start_date)
+    # print("end_date:" + end_date)
+
+    
